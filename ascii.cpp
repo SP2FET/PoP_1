8a41c2c3aa932d017e60f9e8a739b462f2dd6eb6
SP2FET <sp2fet@gmail.com>
2016-12-01 14:13:18 +0100

    ascii

---
+++ ascii.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+    char znak[]= "\u25A0";
+    std::cout << "\u25A0"<<" uni "<< znak<<znak <<"znak"<< std::endl;
+
+  return 0;
+}
--- labirynt.cpp
+++ labirynt.cpp
@@ -1,57 +1,100 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
-const char seg1[][11] = {
-{"####  ####"},
+const short map_size_x = 4; //                                                    rozmiar mapy w poziomie
+const short map_size_y = 2;//                                                     rozmiar mapy w pionie
+const short seg_lenght = 10;
+const short seg_height = 5;
+
+const char seg[][seg_height][seg_lenght+1] = {//                                                     definicja tablicy segmentów
+{{"####  ####"},
 {"##    ####"},
 {"    ##    "},
 {"##      ##"},
-{"####  ####"}};
+{"####  ####"}},
 
-const char seg2[][11] = {
-{"####    ##"},
+
+{{"####    ##"},
 {"  ####  ##"},
 {"          "},
 {"##      ##"},
-{"####    ##"}};
+{"####    ##"}},
 
-const char seg3[][11] = {
-{"          "},
+
+{{"          "},
 {"    ##    "},
 {"    ####  "},
 {"##  ######"},
-{"##        "}};
+{"##        "}},
 
-const char seg4[][11] = {
-{"####      "},
+
+{{"####      "},
 {"##    ##  "},
 {"    ##    "},
 {"  ##    ##"},
-{"##    ###"}};
+{"##     ###"}},
 
-const char seg5[][11] = {
-{"      ####"},
+
+{{"      ####"},
 {"##      ##"},
 {"    ##    "},
 {"    ##    "},
-{"        ##"}};
+{"        ##"}},
 
-const char seg6[][11] = {
-{"####  ####"},
+
+{{"####  ####"},
 {"####  ####"},
 {"          "},
 {"####  ####"},
-{"####  ####"}};
+{"####  ####"}}};
+void make_map(void);
 
 int main(int argc, char const *argv[]) {
 
-for (size_t i = 0; i < 5; i++) {
-  for (size_t j = 0; j < 10; j++) {
-  std::cout << seg1[i][j];
-}
-cout<<endl;
-}
+srand(time(0));
+
+make_map();
+// for (size_t i = 0; i < 6; i++) {
+//
+//   for (size_t j = 0; j < 11; j++) {
+//   std::cout << seg[0][i][j];
+// }
+// cout<<endl;
+// }
 
   return 0;
+}
+
+void make_map(void)
+{
+char c;
+  // char **map = new char*[map_size_y*seg_height];
+  // for (size_t i = 0; i < map_size_x*seg_lenght; i++) {
+  //     *(map+i) = new char[seg_lenght];
+  // }
+//-----------------------------------------------------
+char index[3];
+
+  for (size_t i = 0; i < 5; i++) {
+    index[i] = i;
+  }
+  short temp = 0;
+  for (size_t i = 0; i < 5; i++) {
+
+    for (size_t j = 0; j < 41; j++) {
+
+      if(j > 0 && j%11 == 0) temp++;
+    std::cout << seg[index[temp]][i][j];
+  }
+  temp = 0;
+  std::cout << std::endl;
+}
+  // for (size_t i = 0; i < 10; i++) {
+  //   delete[] map[i];
+  // }
+  // delete[] map;
+
 }
